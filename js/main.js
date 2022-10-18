5c6fb96ab4c9f7055f9bd9f9fc2ca26695b53bc4
skwhit <whitlowspencer@gmail.com>
2022-10-17 17:43:13 -0700

    added hamburger menu and map embed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 import {destinationsArr} from './destinations.js'
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", ()=> {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
 
 const locationsArr = destinationsArr;
 const location = document.querySelector('.location')
@@ -19,5 +31,11 @@ generateDestination.addEventListener("click", ()=>{
     location.innerHTML = `${item.location}, ${item.state} <br><br>
     Scroll down to book a flight and hotel for this destination`;
 
-
+    document.querySelector('.map').src = item.map;
 })
+
+
+
+// api key for google maps AIzaSyA8KtrifgseiVbD7VJ39xQ3LMezaJJ9sBs
+
+
