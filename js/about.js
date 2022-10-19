f9e61b78307dfdaac2b29e821166743da64cf9d6
skwhit <whitlowspencer@gmail.com>
2022-10-19 14:16:14 -0700

    formatted code, added media queries and fixed hamburger menu

--- js/about.js
+++ js/about.js
@@ -1,22 +1,22 @@
-import {destinationsArr} from './destinations.js'
+import { destinationsArr } from "./destinations.js";
 
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
 for (let item of destinationsArr) {
-    const imgContainer = document.createElement('div');
-    container.append(imgContainer);
-    imgContainer.classList.add('imgContainer');
-    
-    const img = document.createElement('img');
-    imgContainer.append(img);
-    img.classList.add('locationImg');
-    img.src = item.image;
+  const imgContainer = document.createElement("div");
+  container.append(imgContainer);
+  imgContainer.classList.add("imgContainer");
 
-    const overlay = document.createElement('div');
-    imgContainer.append(overlay);
-    overlay.classList.add('overlay');
+  const img = document.createElement("img");
+  imgContainer.append(img);
+  img.classList.add("locationImg");
+  img.src = item.image;
 
-    const overlayText = document.createElement('h3');
-    overlay.append(overlayText);
-    overlayText.innerHTML = `${item.location}, ${item.state}`;
+  const overlay = document.createElement("div");
+  imgContainer.append(overlay);
+  overlay.classList.add("overlay");
+
+  const overlayText = document.createElement("h3");
+  overlay.append(overlayText);
+  overlayText.innerHTML = `${item.location}, ${item.state}`;
 }
--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
-import {destinationsArr} from './destinations.js'
+import { destinationsArr } from "./destinations.js";
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-list");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", ()=> {
+document.querySelectorAll(".nav-item").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+  })
+);
 
 const locationsArr = destinationsArr;
-const location = document.querySelector('.location')
+const location = document.querySelector(".location");
 let randomNum;
 let item;
 // let opacity;
 // let inervalID;
 
-const generateDestination = document.querySelector('.generateDestination');
-generateDestination.addEventListener("click", ()=>{
-    randomNum = Math.floor(Math.random()*locationsArr.length)
-    item = locationsArr[randomNum]
-    
-    console.log(item);
-    document.querySelector('style').innerHTML = 
-    `.destinationContainer::before {background-image: url(${item.image})}`
-    location.innerHTML = `${item.location}, ${item.state} <br><br>
+const generateDestination = document.querySelector(".generateDestination");
+generateDestination.addEventListener("click", () => {
+  randomNum = Math.floor(Math.random() * locationsArr.length);
+  item = locationsArr[randomNum];
+
+  console.log(item);
+  document.querySelector(
+    "style"
+  ).innerHTML = `.destinationContainer::before {background-image: url(${item.image})}`;
+  location.innerHTML = `${item.location}, ${item.state} <br><br>
     Scroll down to book a flight and hotel for this destination`;
 
-    document.querySelector('.map').src = item.map;
-})
-
-
+  document.querySelector(".map").src = item.map;
+});
 
 // api key for google maps AIzaSyA8KtrifgseiVbD7VJ39xQ3LMezaJJ9sBs
-
-
