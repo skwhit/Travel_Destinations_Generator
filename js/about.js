040b0622d15a739a779b35c0d1d951dc2f36acfe
skwhit <whitlowspencer@gmail.com>
2022-10-18 23:48:51 -0700

    created second page to display all locations

---
+++ js/about.js
@@ -0,0 +1,22 @@
+import {destinationsArr} from './destinations.js'
+
+const container = document.querySelector('.container');
+
+for (let item of destinationsArr) {
+    const imgContainer = document.createElement('div');
+    container.append(imgContainer);
+    imgContainer.classList.add('imgContainer');
+    
+    const img = document.createElement('img');
+    imgContainer.append(img);
+    img.classList.add('locationImg');
+    img.src = item.image;
+
+    const overlay = document.createElement('div');
+    imgContainer.append(overlay);
+    overlay.classList.add('overlay');
+
+    const overlayText = document.createElement('h3');
+    overlay.append(overlayText);
+    overlayText.innerHTML = `${item.location}, ${item.state}`;
+}
