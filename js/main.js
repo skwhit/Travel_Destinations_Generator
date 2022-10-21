9eff9ffcb0b78ec5261262022369a7e1a07e4031
skwhit <whitlowspencer@gmail.com>
2022-10-20 17:44:59 -0700

    updated image sizes. created javascript image transition effect

--- js/main.js
+++ js/main.js
@@ -17,24 +17,36 @@ document.querySelectorAll(".nav-item").forEach((n) =>
 
 const locationsArr = destinationsArr;
 const location = document.querySelector(".location");
+const destinationContainer = document.querySelector(".destinationContainer");
+const map = document.querySelector(".map");
 let randomNum;
 let item;
-// let opacity;
-// let inervalID;
 
 const generateDestination = document.querySelector(".generateDestination");
 generateDestination.addEventListener("click", () => {
   randomNum = Math.floor(Math.random() * locationsArr.length);
   item = locationsArr[randomNum];
-
   console.log(item);
-  document.querySelector(
-    "style"
-  ).innerHTML = `.destinationContainer::before {background-image: url(${item.image});}`;
-  location.innerHTML = `${item.location}, ${item.state} <br><br>
-    Scroll down to book a flight and hotel for this destination`;
 
-  document.querySelector(".map").src = item.map;
+  //fade  effect initialized imediately
+  document.body.classList.toggle("fade");
+
+  //map will change after .5 seconds
+  setTimeout(() => {
+    map.src = item.map;
+  }, 500);
+
+  //image will be updated after 1 second
+  setTimeout(() => {
+    destinationContainer.style.backgroundImage = `url(${item.image})`;
+    location.innerHTML = `${item.location}, ${item.state} <br><br>
+    Scroll down to book a flight and hotel for this destination`;
+  }, 1000);
+
+  //fade class removed after 2 seconds when fade animation is complete
+  setTimeout(() => {
+    document.body.classList.toggle("fade");
+  }, 2000);
 });
 
 // api key for google maps AIzaSyA8KtrifgseiVbD7VJ39xQ3LMezaJJ9sBs
